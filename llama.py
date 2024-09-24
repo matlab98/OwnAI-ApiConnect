a70fe2436939ae63abd75d543625ae68c3afe165
Hilder Arrieta <38758814+matlab98@users.noreply.github.com>
2024-09-24 06:04:42 -0500

    Update llama.py

--- llama.py
+++ llama.py
@@ -1,8 +1,6 @@
 import requests
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # Obtener el secreto desde las variables de entorno
 LLAMA_API_KEY = os.getenv("LLAMA_API_KEY")
 API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3.1-8B"
