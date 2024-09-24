eaec9a165c88e68774c2b652f9518bd6e28da0f3
Hilder Arrieta <38758814+matlab98@users.noreply.github.com>
2024-09-24 05:59:36 -0500

    Update llama.py

--- llama.py
+++ llama.py
@@ -1,7 +1,12 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+# Obtener el secreto desde las variables de entorno
+LLAMA_API_KEY = os.getenv("LLAMA_API_KEY")
 API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3.1-8B"
-headers = {"Authorization": "Bearer {{LLAMA_API_KEY}}"}
+headers = {"Authorization": f"Bearer {LLAMA_API_KEY}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
